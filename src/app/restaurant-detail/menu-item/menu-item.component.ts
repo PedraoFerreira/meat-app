bbb30110d530ce58e84d864eca2c257f54591e85
Pedro Ferreira da Silva Junior <pedro.ferreira@grupohk.local>
2018-07-18 17:57:10 -0300

    Aula 50

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from '../../restaurants/restaurants.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'mt-menu-item',
@@ -6,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuItemComponent implements OnInit {
 
-  constructor() { }
+  menuItens: Observable<any>
+
+  constructor(private restaurantsService: RestaurantsService,
+              private route: ActivatedRoute) { }
+
 
   ngOnInit() {
+    this.menuItens = this.restaurantsService.menuOfRestaurant(this.route.parent.snapshot.params['id'])
   }
 
 }
